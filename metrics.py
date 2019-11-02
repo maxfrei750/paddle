792dd75cbd91a3c18f2f5f9761c4a17ebc9bf2b5
Max Frei <max.frei@uni-due.de>
2019-11-02 02:42:08 +0100

    Added ignite version of the training script.

---
+++ metrics.py
@@ -0,0 +1,121 @@
+from ignite.metrics import Metric
+from ignite.exceptions import NotComputableError
+from torchvision_detection_references.coco_eval import CocoEvaluator
+from torchvision_detection_references.coco_utils import get_coco_api_from_dataset
+import torch
+import numpy as np
+
+
+class _CocoEvaluator(CocoEvaluator):
+    def __init__(self, data_loader):
+        self.coco = get_coco_api_from_dataset(data_loader.dataset)
+        self.iou_type = "segm"  # _get_iou_types(model)
+
+        super().__init__(self.coco, [self.iou_type])
+
+        self.parameters = self.coco_eval[self.iou_type].params
+
+    def calculate_average_precision(self, iou_threshold=None, area_range="all", n_detections_max=100):
+        coco_eval = self.coco_eval[self.iou_type]
+
+        p = self.parameters
+
+        aind = [i for i, aRng in enumerate(p.areaRngLbl) if aRng == area_range]
+        mind = [i for i, mDet in enumerate(p.maxDets) if mDet == n_detections_max]
+
+        # dimension of precision: [TxRxKxAxM]
+        s = coco_eval.eval['precision']
+        # IoU
+        if iou_threshold is not None:
+            t = np.where(iou_threshold == p.iouThrs)[0]
+            s = s[t]
+        s = s[:, :, :, aind, mind]
+
+        if len(s[s > -1]) == 0:
+            mean_s = -1
+        else:
+            mean_s = np.mean(s[s > -1])
+
+        return mean_s
+
+
+class AveragePrecision(Metric):
+    """
+    Calculate coco evaluation.
+    """
+
+    def __init__(self, data_loader, device, iou_threshold=None, area_range="all", n_determinations_max=100):
+        self._data_loader = data_loader
+        self._device = device
+        self._coco_evaluator = None
+        self.iou_threshold = iou_threshold
+        self.area_range = area_range
+        self.n_determinations_max = n_determinations_max
+        self._has_data = False
+        self.parameters = None
+        self.value = None
+
+        super().__init__()
+
+    def reset(self):
+        self._has_data = False
+        self._coco_evaluator = _CocoEvaluator(self._data_loader)
+        self.parameters = self._coco_evaluator.parameters
+        self.value = None
+
+    def update(self, state):
+        self._has_data = True
+        n_threads = torch.get_num_threads()
+        # FIXME remove this and make paste_masks_in_image run on the GPU
+        torch.set_num_threads(1)
+
+        cpu_device = torch.device("cpu")
+
+        outputs, targets = state
+
+        targets = [{k: v.to(self._device) for k, v in t.items()} for t in targets]
+        outputs = [{k: v.to(cpu_device) for k, v in t.items()} for t in outputs]
+
+        res = {target["image_id"].item(): output for target, output in zip(targets, outputs)}
+
+        self._coco_evaluator.update(res)
+        torch.set_num_threads(n_threads)
+
+    def compute(self):
+        if not self._has_data:
+            raise NotComputableError("Average precision must have at least one sample before it can be computed.")
+
+        n_threads = torch.get_num_threads()
+        # FIXME remove this and make paste_masks_in_image run on the GPU
+        torch.set_num_threads(1)
+
+        self._coco_evaluator.synchronize_between_processes()
+        self._coco_evaluator.accumulate()
+        average_precision = \
+            self._coco_evaluator.calculate_average_precision(iou_threshold=self.iou_threshold,
+                                                             area_range=self.area_range,
+                                                             n_detections_max=self.n_determinations_max)
+
+        torch.set_num_threads(n_threads)
+
+        self.value = average_precision
+
+        return average_precision
+
+    def print(self):
+        p = self.parameters
+
+        format_string = ' {:<18} {} @[ IoU={:<9} | area={:>6s} | n_detections_max={:>3d} ] = {:0.3f}'
+        title_string = 'Average Precision'
+        type_string = '(AP)'
+        iou_string = '{:0.2f}:{:0.2f}'.format(p.iouThrs[0], p.iouThrs[-1]) \
+            if self.iou_threshold is None else '{:0.2f}'.format(self.iou_threshold)
+
+        print(format_string.format(title_string,
+                                   type_string,
+                                   iou_string,
+                                   self.area_range,
+                                   self.n_determinations_max,
+                                   self.value
+                                   )
+              )
---
+++ train_model_ignite.py
@@ -0,0 +1,160 @@
+import torch
+from torch.utils.data import DataLoader
+from torchvision_detection_references.utils import collate_fn
+import torchvision_detection_references.transforms as T
+from dataset import Dataset
+from os import path
+from utilities import set_random_seed
+from mask_rcnn import get_mask_rcnn_model, create_mask_rcnn_trainer
+from ignite.engine import create_supervised_evaluator, Events
+from metrics import AveragePrecision
+
+
+def main():
+    # Parameters -------------------------------------------------------------------------------------------------------
+    n_classes = 2  # Background and Fiber
+
+    batch_size_train = 4
+    batch_size_val = 1
+    max_epochs = 10
+    random_seed = 12345
+    print_frequency = 10
+
+    subset_train = "training"
+    subset_val = "validation"
+
+    subset_train += "mini"
+    subset_val += "mini"
+
+    # Reproducibility --------------------------------------------------------------------------------------------------
+    set_random_seed(random_seed)
+
+    # Model ------------------------------------------------------------------------------------------------------------
+    model = get_mask_rcnn_model(n_classes)
+
+    # Paths ------------------------------------------------------------------------------------------------------------
+    data_root = path.join("D:\\", "sciebo", "Dissertation", "Referenzdaten", "IUTA", "easy_images",
+                          "individual_fibers_no_clutter_no_loops")
+
+    # Data -------------------------------------------------------------------------------------------------------------
+    # TODO: Test pillow-SIMD
+    data_loader_train, data_loader_val = \
+        get_data_loaders(data_root,
+                         subset_train=subset_train, subset_val=subset_val,
+                         batch_size_train=batch_size_train, batch_size_val=batch_size_val)
+
+    # Device -----------------------------------------------------------------------------------------------------------
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    model = model.to(device)
+
+    # Optimizer --------------------------------------------------------------------------------------------------------
+    parameters = [p for p in model.parameters() if p.requires_grad]
+    optimizer = torch.optim.SGD(parameters, lr=0.005, momentum=0.9, weight_decay=0.0005)
+    # optimizer = torch.optim.Adam(parameters, lr=1e-3)
+
+    # Trainer ----------------------------------------------------------------------------------------------------------
+    trainer = create_mask_rcnn_trainer(model, optimizer, data_loader_train, device)
+
+    # Learning rate scheduler ------------------------------------------------------------------------------------------
+    lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer,
+                                                   step_size=3,
+                                                   gamma=0.1)
+
+    @trainer.on(Events.EPOCH_COMPLETED)
+    def step_lr_scheduler(engine):
+        lr_scheduler.step()
+
+    # Evaluation -------------------------------------------------------------------------------------------------------
+    metrics = {
+        "AP": AveragePrecision(data_loader_val, device)
+    }
+
+    evaluator_val = create_supervised_evaluator(model, metrics=metrics, device=device)
+
+    @trainer.on(Events.EPOCH_COMPLETED)
+    def evaluate_validation_set(engine):
+        print(" Validation:")
+        evaluator_val.run(data_loader_val)
+        metrics["AP"].print()
+
+    # Logging ----------------------------------------------------------------------------------------------------------
+    @trainer.on(Events.EPOCH_STARTED)
+    def log_epoch(engine):
+        epoch = engine.state.epoch
+
+        print("\nEpoch: {}".format(epoch))
+        print(" Training:")
+
+        if epoch == 1:
+            engine.state.previous_epoch = 0
+
+        if engine.state.previous_epoch != epoch:
+            engine.state.epoch_iteration = 0
+            engine.state.previous_epoch = epoch
+
+    @trainer.on(Events.ITERATION_STARTED)
+    def increment_(engine):
+        engine.state.epoch_iteration += 1
+
+    @trainer.on(Events.ITERATION_COMPLETED)
+    def log_training_losses(engine):
+        epoch_iteration = engine.state.epoch_iteration
+
+        if (epoch_iteration - 1) % print_frequency == 0:
+            output = engine.state.output
+            delimiter = "    "
+
+            log_items = list()
+            log_items.append(" [{:4d}]".format(epoch_iteration))
+            log_items += ["{}: {:.4f}".format(key, output[key]) for key in output]
+
+            log_msg = delimiter.join(log_items)
+            print(log_msg)
+
+    # Training ---------------------------------------------------------------------------------------------------------
+    try:
+        trainer.run(data_loader_train, max_epochs=max_epochs)
+    finally:
+        pass
+        # tensorboard_writer.close()
+
+
+def get_transform(train):
+    transforms = [T.ToTensor()]
+
+    if train:
+        transforms.append(T.RandomHorizontalFlip(0.5))
+        transforms.append(T.RandomVerticalFlip(0.5))
+    return T.Compose(transforms)
+
+
+def get_data_loaders(data_root, subset_train="training", subset_val="validation", batch_size_train=1, batch_size_val=1):
+    class_name_dict = {
+        1: "fiber"
+    }
+
+    dataset_train = Dataset(data_root,
+                            subset_train,
+                            transforms=get_transform(train=True),
+                            class_name_dict=class_name_dict)
+    data_loader_train = DataLoader(dataset_train,
+                                   batch_size=batch_size_train,
+                                   shuffle=True,
+                                   num_workers=4,
+                                   collate_fn=collate_fn)
+
+    dataset_val = Dataset(data_root,
+                          subset_val,
+                          transforms=get_transform(train=False),
+                          class_name_dict=class_name_dict)
+    data_loader_val = DataLoader(dataset_val,
+                                 batch_size=batch_size_val,
+                                 shuffle=False,
+                                 num_workers=4,
+                                 collate_fn=collate_fn)
+
+    return data_loader_train, data_loader_val
+
+
+if __name__ == "__main__":
+    main()
