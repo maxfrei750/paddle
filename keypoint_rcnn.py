3184df22132ca765d08c9f34592089afc021eab6
Max Frei <max.frei@uni-due.de>
2019-11-11 13:02:35 +0100

    Exchanged backbone with resnet50.

--- keypoint_rcnn.py
+++ keypoint_rcnn.py
@@ -5,31 +5,43 @@ import math
 import sys
 from ignite.engine import Engine
 import torchvision_detection_references.utils as utils
+from torch import nn
 
 
 def get_keypoint_rcnn_model(num_classes, num_keypoints):
-    # load a pre-trained model for classification and return
-    # only the features
-    # TODO: Exchange backbone.
-    backbone = torchvision.models.mobilenet_v2(pretrained=True).features
-    # backbone = torchvision.models.resnet50(pretrained=True).features
-    # KeypointRCNN needs to know the number of
-    # output channels in a backbone. For mobilenet_v2, it's 1280
-    # so we need to add it here
-    backbone.out_channels = 1280
+    # Load a pre-trained model for classification and return only the features.
+    resnet50 = torchvision.models.resnet50(pretrained=True)
 
-    # let's make the RPN generate 5 x 3 anchors per spatial
+    layers = [
+        resnet50.conv1,
+        resnet50.bn1,
+        resnet50.relu,
+        resnet50.maxpool,
+        resnet50.layer1,
+        resnet50.layer2,
+        resnet50.layer3,
+        resnet50.layer4,
+        resnet50.avgpool]
+
+    backbone = nn.Sequential(*layers)
+
+    # KeypointRCNN needs to know the number of
+    # output channels in a backbone, so we need to add it here.
+    backbone.out_channels = resnet50.inplanes
+
+    # Let's make the RPN generate 5 x 3 anchors per spatial
     # location, with 5 different sizes and 3 different aspect
     # ratios. We have a Tuple[Tuple[int]] because each feature
     # map could potentially have different sizes and
-    # aspect ratios
+    # aspect ratios.
+    # TODO: Adjust ROI sizes.
     anchor_generator = AnchorGenerator(sizes=((32, 64, 128, 256, 512),),
                                        aspect_ratios=((0.5, 1.0, 2.0),))
 
-    # let's define what are the feature maps that we will
+    # Let's define what are the feature maps that we will
     # use to perform the region of interest cropping, as well as
     # the size of the crop after rescaling.
-    # if your backbone returns a Tensor, featmap_names is expected to
+    # If your backbone returns a Tensor, featmap_names is expected to
     # be [0]. More generally, the backbone should return an
     # OrderedDict[Tensor], and in featmap_names you can choose which
     # feature maps to use.
@@ -40,7 +52,7 @@ def get_keypoint_rcnn_model(num_classes, num_keypoints):
     keypoint_roi_pooler = torchvision.ops.MultiScaleRoIAlign(featmap_names=[0],
                                                              output_size=14,
                                                              sampling_ratio=2)
-    # put the pieces together inside a FasterRCNN model
+    # Put the pieces together inside a FasterRCNN model.
     model = KeypointRCNN(backbone,
                          num_classes=num_classes,
                          rpn_anchor_generator=anchor_generator,
