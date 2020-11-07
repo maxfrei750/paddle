06541342b51948a0dc6ce43c257b7dff9b44354f
Max Frei <max.frei@uni-due.de>
2020-11-07 01:01:18 +0100

    Added more augmentations.

--- train_model.py
+++ train_model.py
@@ -126,8 +126,12 @@ def main():
 
 def get_transform():
     transform = albumentations.Compose(
-        [albumentations.RandomCrop(width=1024, height=1024)],
-        bbox_params=albumentations.BboxParams(format="pascal_voc", label_fields=["class_labels"]),
+        [
+            albumentations.RandomCrop(width=1024, height=1024),
+            albumentations.HorizontalFlip(0.5),
+            albumentations.VerticalFlip(0.5),
+            albumentations.RandomRotate90(0.75),
+        ]
     )
 
     return transform
