5e9300f8282042cf7847fcd3fb6b31bf4b027ad6
Max Frei <max.frei@uni-due.de>
2020-11-10 08:44:39 +0100

    Fixed checkpoint file ending in get_best_model_path.

--- utilities.py
+++ utilities.py
@@ -14,7 +14,7 @@ def get_time_stamp():
 
 
 def get_best_model_path(log_dir, filename_prefix):
-    log_files = glob(path.join(log_dir, filename_prefix + "*.pth"))
+    log_files = glob(path.join(log_dir, filename_prefix + "*.pt"))
     val_accuracies = [float(re.search("=(\d.\d+)", log_file).group(1)) for log_file in log_files]
     best_index = np.argmax(val_accuracies)
     best_model_path = log_files[best_index]
