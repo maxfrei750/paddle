077b45cb32651272e57d213127310f4f3a6d0b1b
Max Frei <max.frei@uni-due.de>
2020-11-12 15:11:43 +0100

    Added function: get_viridis_colors

--- visualization.py
+++ visualization.py
@@ -3,6 +3,7 @@ import warnings
 
 import numpy as np
 from matplotlib import cm
+from matplotlib.colors import LinearSegmentedColormap
 from PIL import Image, ImageDraw, ImageFont, ImageOps
 from scipy.ndimage.morphology import binary_erosion
 from skimage import img_as_float, img_as_ubyte
@@ -10,6 +11,24 @@ from skimage import img_as_float, img_as_ubyte
 import torch
 from data import extract_bounding_box
 from torchvision import transforms
+
+
+def get_viridis_colors(num_colors):
+    color_min = (0.231, 0.322, 0.545)
+    color_max = (0.369, 0.788, 0.384)
+
+    if num_colors == 1:
+        return color_min
+
+    colormap = LinearSegmentedColormap.from_list("custom_viridis", colors=[color_min, color_max])
+
+    colors = []
+
+    for i in range(num_colors):
+        color = colormap(i / (num_colors - 1))[:3]
+        colors.append(color)
+
+    return colors
 
 
 def get_random_colors(num_colors):
