21e82bd5e85df7fa07290a5ee7b2a5a9c362e18a
Max Frei <max.frei@uni-due.de>
2020-12-14 11:04:58 +0100

    inspect_model: Use fire and refactoring.

--- inspect_model.py
+++ inspect_model.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
+import fire
 import matplotlib.pyplot as plt
 from torch.utils.data import DataLoader
 
@@ -7,24 +8,37 @@ from data import Dataset
 from deployment import analyze_image, load_trained_model
 from postprocessing import calculate_area_equivalent_diameters, filter_border_particles
 from transforms import get_transform
-from utilities import get_best_model_path, log_parameters_as_yaml, set_random_seed
+from utilities import AnyPath, get_best_checkpoint_path, log_parameters_as_yaml, set_random_seed
 from visualization import plot_particle_size_distributions, save_visualization
 
 
-def inspect_model():
-    score_threshold = 0.5
-    num_subsamples_per_image = 10
-    random_seed = 42
-    log_root = "logs"
-    model_root = "maskrcnn_2020-11-10_17-31-25"
-    data_root = "data"
-    subset_validation = "validation"
-    device = "cuda"
+def inspect_model(
+    model_id: str,
+    log_root: AnyPath = "logs",
+    data_root: AnyPath = "data",
+    subset: str = "validation",
+    num_subsamples_per_image: int = 1,
+    score_threshold: float = 0.5,
+    random_seed: int = 42,
+    device: str = "cuda",
+):
+    """Inspect a model by applying it to a validation dataset and store the results in the model folder.
+
+    :param model_id: Identifier of the model.
+    :param log_root: Path of the log folder. Default: "logs"
+    :param data_root: Path of the data folder. Default: "data"
+    :param subset: Name of the subset to use for the validation. Default: "validation"
+    :param cropping_rectangle: If not None, rectangle to use for the cropping of the validation images. Default: None
+    :param num_subsamples_per_image: Number of images that are sampled from each validation sample image. Default: 1
+    :param score_threshold: Detections with a score<score_threshold are discarded. Default: 0.5
+    :param random_seed: Random seed for random, numpy and torch. Default: 42
+    :param device: Computation device. Default: "cuda"
+    """
 
     set_random_seed(random_seed)
 
-    model_folder_path = Path(log_root) / model_root
-    result_folder_path = model_folder_path / "results" / subset_validation
+    model_folder_path = Path(log_root) / model_id
+    result_folder_path = model_folder_path / "results" / subset
     visualization_image_folder_path = result_folder_path / "images"
 
     result_folder_path.mkdir(exist_ok=True, parents=True)
@@ -39,10 +53,10 @@ def inspect_model():
         random_seed=random_seed,
     )
 
-    model_path = get_best_model_path(model_folder_path, "model_")
+    model_path = get_best_checkpoint_path(model_folder_path, "model")
     model = load_trained_model(model_path, device)
 
-    dataset_gt = Dataset(data_root, subset_validation)
+    dataset_gt = Dataset(data_root, subset)
 
     masks_gt = []
     for _, target in dataset_gt:
@@ -53,12 +67,15 @@ def inspect_model():
     masks_pred = []
     scores_pred = []
 
-    dataset_pred = Dataset(data_root, subset_validation, transforms=get_transform(training=False))
+    dataset_pred = Dataset(data_root, subset, transforms=get_transform(training=False))
 
-    dataloader = DataLoader(dataset_pred, batch_size=1)
-    dataiterator = iter(dataloader)
+    dataloader_pred = DataLoader(
+        dataset_pred,
+        batch_size=1,
+    )
+    dataiterator = iter(dataloader_pred)
 
-    num_images = len(dataloader)
+    num_images = len(dataloader_pred)
 
     num_subsamples = num_subsamples_per_image * num_images
 
@@ -68,7 +85,7 @@ def inspect_model():
         except StopIteration:
             # StopIteration is thrown if dataset ends
             # reinitialize data loader
-            dataiterator = iter(dataloader)
+            dataiterator = iter(dataloader_pred)
             image, target = next(dataiterator)
 
         prediction = analyze_image(model, image)
@@ -105,4 +122,4 @@ def inspect_model():
 
 
 if __name__ == "__main__":
-    inspect_model()
+    fire.Fire(inspect_model)
