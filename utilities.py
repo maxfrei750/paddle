e07aa1c000443cc10dc3eef31471335ca51f0e6e
Max Frei <max.frei@uni-due.de>
2020-12-14 10:33:30 +0100

    Refactored module: utilities

--- utilities.py
+++ utilities.py
@@ -2,43 +2,69 @@ import os
 import random
 import re
 from datetime import datetime
-from glob import glob
-from os import path
 from pathlib import Path
-from typing import Union
+from typing import Any, Union
 
 import numpy as np
 import torch
 import yaml
 
+AnyPath = Union[str, bytes, os.PathLike]
 
-def get_time_stamp():
+
+def get_time_stamp() -> str:
+    """Get current time formatted as string.
+
+    :return: String representing the current time in the following format: %Y-%m-%d_%H-%M-%S
+    """
     return datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 
 
-def get_best_model_path(log_dir, filename_prefix):
-    log_files = glob(path.join(log_dir, filename_prefix + "*.pt"))
+def get_best_checkpoint_path(log_dir: AnyPath, filename_prefix: str) -> AnyPath:
+    """Retrieve model checkpoint with the highest accuracy.
+
+    :param log_dir: Path where model checkpoints are saved.
+    :param filename_prefix: Checkpoint prefix.
+    :return: Path of the checkpoint with the highest accuracy.
+    """
+    log_dir = Path(log_dir)
+    log_file_paths = list(log_dir.glob(filename_prefix + "*.pt"))
     val_accuracies = [
-        float(re.search(r"=([+-]?((\d+\.?\d*)|(\.\d+)))", log_file)[1]) for log_file in log_files
+        float(re.search(r"=([+-]?((\d+\.?\d*)|(\.\d+)))", str(log_file))[1])
+        for log_file in log_file_paths
     ]
-    best_index = np.argmax(val_accuracies)
-    best_model_path = log_files[best_index]
+    best_index = int(np.argmax(val_accuracies))
+    best_model_path = log_file_paths[best_index]
     return best_model_path
 
 
-def get_last_checkpoint_path(log_dir: Union[str, bytes, os.PathLike]):
+def get_last_checkpoint_path(log_dir: AnyPath) -> AnyPath:
+    """Retrieve last model checkpoint.
+
+    :param log_dir: Path where model checkpoints are saved.
+    :return: Path of the last model checkpoint.
+    """
     log_dir = Path(log_dir)
     log_file_paths = sorted(list(log_dir.glob("checkpoint_*_*.pt")))
     last_checkpoint_path = log_file_paths[-1]
     return last_checkpoint_path
 
 
-def set_random_seed(seed):
+def set_random_seed(seed: int):
+    """Set the random seeds of the random, numpy and torch.
+
+    :param seed: Random seed.
+    """
     random.seed(seed)
     np.random.seed(seed)
     torch.manual_seed(seed)
 
 
-def log_parameters_as_yaml(yaml_file_path, **kwargs):
+def log_parameters_as_yaml(yaml_file_path: AnyPath, **kwargs: Any):
+    """Store all optional parameters to a yaml file.
+
+    :param yaml_file_path: Path of the output yaml file.
+    :param kwargs: Dictionary of optional parameters
+    """
     with open(yaml_file_path, "w") as file:
         yaml.dump(kwargs, file)
