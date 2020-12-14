e72a3b483c7f46d166c47a5e0aeb3da8b9cfa4a9
Max Frei <max.frei@uni-due.de>
2020-12-14 10:29:20 +0100

    Added function: utilities.get_last_checkpoint_path

--- utilities.py
+++ utilities.py
@@ -1,8 +1,11 @@
+import os
 import random
 import re
 from datetime import datetime
 from glob import glob
 from os import path
+from pathlib import Path
+from typing import Union
 
 import numpy as np
 import torch
@@ -21,6 +24,13 @@ def get_best_model_path(log_dir, filename_prefix):
     best_index = np.argmax(val_accuracies)
     best_model_path = log_files[best_index]
     return best_model_path
+
+
+def get_last_checkpoint_path(log_dir: Union[str, bytes, os.PathLike]):
+    log_dir = Path(log_dir)
+    log_file_paths = sorted(list(log_dir.glob("checkpoint_*_*.pt")))
+    last_checkpoint_path = log_file_paths[-1]
+    return last_checkpoint_path
 
 
 def set_random_seed(seed):
