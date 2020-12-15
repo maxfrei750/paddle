7629576dba973f5b6a525a6733257787d1a7e97e
Max Frei <max.frei@uni-due.de>
2020-12-15 22:12:05 +0100

    Cleaned up code.

--- train_model.py
+++ train_model.py
@@ -1,15 +1,22 @@
-from os import path
+from pathlib import Path
 
+import fire
 from config import Config
 from training import training
 
 
-def main(config_name="maskrcnn"):
-    config = Config.load(path.join("configs", config_name + ".yml"))
+def train_model(config_name="maskrcnn"):
+    """Trains a default model.
+
+    :param config_name: File name of a configuration file (without yml-extension).
+    """
+    config_file_name = config_name + ".yml"
+    config_path = Path("configs") / config_file_name
+    config = Config.load(config_path)
     config["name"] = config_name
 
     training(config)
 
 
 if __name__ == "__main__":
-    main()
+    fire.Fire(train_model)
