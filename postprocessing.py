5776ddf3695b8acab9c0303324b9aeeea63acde8
Max Frei <max.frei@uni-due.de>
2021-01-04 15:55:29 +0100

    Functions to calculate minimum and maximum feret diameters of masks.

--- postprocessing.py
+++ postprocessing.py
@@ -1,6 +1,7 @@
 from itertools import compress
 
 import numpy as np
+from diplib.PyDIP_bin import MeasurementTool as PyDipMeasurementTool
 from skimage.segmentation import clear_border
 
 
@@ -37,3 +38,28 @@ def calculate_area_equivalent_diameters(masks):
     masks = masks.reshape(masks.shape[0], -1)
     areas = np.asarray(masks).sum(axis=1)
     return list(np.sqrt(4 * areas / np.pi))
+
+
+def calculate_minimum_and_maximum_feret_diameter(mask):
+    mask = mask.astype(bool).astype("uint16")
+    measurement = PyDipMeasurementTool.Measure(mask, mask, ["Feret"])
+    feret_diameter_max = measurement[1]["Feret"][0]
+    feret_diameter_min = measurement[1]["Feret"][1]
+
+    return feret_diameter_min, feret_diameter_max
+
+
+def calculate_minimum_feret_diameter(mask):
+    return calculate_minimum_and_maximum_feret_diameter(mask)[0]
+
+
+def calculate_maximum_feret_diameter(mask):
+    return calculate_minimum_and_maximum_feret_diameter(mask)[1]
+
+
+def calculate_minimum_feret_diameters(masks):
+    return [calculate_minimum_feret_diameter(mask) for mask in masks]
+
+
+def calculate_maximum_feret_diameters(masks):
+    return [calculate_maximum_feret_diameter(mask) for mask in masks]
