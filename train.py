3bb5b4535dd430d2e86b8df932ab665c38091f75
Max Frei <max.frei@uni-due.de>
2021-01-27 18:08:57 +0100

    Cleaned up code.

--- train.py
+++ train.py
@@ -2,10 +2,12 @@ import logging
 import os
 import warnings
 from pathlib import Path
+from typing import Tuple
 
 import hydra
 import pytorch_lightning as pl
 from omegaconf import DictConfig, OmegaConf
+from pytorch_lightning import seed_everything
 from pytorch_lightning.callbacks import EarlyStopping, LearningRateMonitor, ModelCheckpoint
 from pytorch_lightning.loggers import TensorBoardLogger
 
@@ -13,27 +15,36 @@ from callbacks import ExampleDetectionMonitor
 from data import MaskRCNNDataModule
 from maskrcnntrainer import MaskRCNNTrainer
 from models import LightningMaskRCNN
-
-# TODO: Docstrings
-# TODO: Use typing.
+from utilities import AnyPath
 
 
 @hydra.main(config_path="configs", config_name="maskrcnn")
 def train(config: DictConfig) -> None:
+    """Trains a Mask R-CNN based on a given config.
+
+    :param config: OmegaConf dictionary.
+    """
+
+    # TODO: Remove this filter, as soon as torchvision>0.8.2 is released.
+    warnings.filterwarnings(
+        "ignore",
+        message="The default behavior for interpolate/upsample with float scale_factor changed",
+    )
+
     log_root, version = setup_hydra()
 
     logging.getLogger(__name__).info(
         f"Training with the following config:\n{OmegaConf.to_yaml(config)}"
     )
 
-    pl.seed_everything(config.program.random_seed)
+    seed_everything(config.program.random_seed)
 
     data_module = MaskRCNNDataModule(**config.datamodule)
 
     model = LightningMaskRCNN(**config.model)
 
     if config.program.search_optimum_learning_rate:
-        lr_tuner = pl.Trainer(auto_lr_find=True)
+        lr_tuner = MaskRCNNTrainer(auto_lr_find=True, **config.trainer)
         lr_tuner.tune(model, datamodule=data_module)
         exit()
 
@@ -52,16 +63,16 @@ def train(config: DictConfig) -> None:
         **config.trainer,
     )
 
-    # TODO: Remove this filter, as soon as torchvision>0.8.2 is released.
-    warnings.filterwarnings(
-        "ignore",
-        message="The default behavior for interpolate/upsample with float scale_factor changed",
-    )
-
     trainer.fit(model, datamodule=data_module)
 
 
-def setup_hydra():
+def setup_hydra() -> Tuple[AnyPath, str]:
+    """Hydra automatically changes the working directory to the logging directory. This function
+        extracts the log_root and version string needed by lightning and restores the original
+        working directory.
+
+    :return: Path where logs are stored and version string.
+    """
     log_dir = Path.cwd()
     log_root = log_dir.parent
     version = log_dir.name
