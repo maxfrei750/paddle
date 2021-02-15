e2c74e2cdcb177a2ece2799ca884fed879296b18
Max Frei <max.frei@uni-due.de>
2021-02-15 12:09:22 +0100

    Moved inspect_model to paddle.inspection module.

--- paddle/inspection.py
+++ paddle/inspection.py
@@ -1,8 +1,14 @@
 from pathlib import Path
-from typing import Dict, Optional
+from typing import Dict, List, Optional
 
+from pytorch_lightning import Trainer
+
+from .callbacks import TestPredictionWriter
 from .custom_types import AnyPath
+from .data import MaskRCNNDataModule
+from .models import LightningMaskRCNN
 from .postprocessing import Postprocessor, SaveVisualization
+from .utilities import get_best_checkpoint_path, get_latest_log_folder_path
 
 
 def inspect_dataset(
@@ -59,3 +65,59 @@ def inspect_dataset(
     Postprocessor(
         data_root, subset, post_processing_steps, progress_bar_description="Visualization: "
     ).run()
+
+
+def inspect_model(
+    log_root: AnyPath,
+    data_root: AnyPath,
+    subset: str,
+    model_id: Optional[str] = None,
+    cropping_rectangle: Optional[List[int]] = None,
+):
+    """Inspect a model by applying it to a validation dataset and store the results in the model
+        folder.
+
+    :param model_id: Identifier of the model or None. If None, then the latest trained model is
+        inspected.
+    :param log_root: Path of the log folder.
+    :param data_root: Path of the data folder.
+    :param subset: Name of the subset to use for the validation.
+    :param cropping_rectangle: If not None, rectangle to use for the cropping of the validation
+        images.
+    """
+
+    if model_id is None:
+        model_id = get_latest_log_folder_path(log_root)
+
+    model_root = Path(log_root) / model_id
+    result_root = model_root / "results"
+    checkpoint_root = model_root / "checkpoints"
+
+    checkpoint_path = get_best_checkpoint_path(checkpoint_root)
+    model = LightningMaskRCNN.load_from_checkpoint(checkpoint_path)
+
+    data_module = MaskRCNNDataModule(
+        data_root, cropping_rectangle=cropping_rectangle, test_subset=subset
+    )
+    data_module.setup()
+
+    map_label_to_class_name = data_module.test_dataset.map_label_to_class_name
+
+    trainer = Trainer(
+        logger=False,
+        checkpoint_callback=False,
+        gpus=-1,
+        callbacks=[
+            TestPredictionWriter(
+                result_root / subset, map_label_to_class_name=map_label_to_class_name
+            )
+        ],
+    )
+
+    trainer.test(model=model, datamodule=data_module, verbose=False)
+
+    inspect_dataset(
+        result_root,
+        subset,
+        map_label_to_class_name=map_label_to_class_name,
+    )
