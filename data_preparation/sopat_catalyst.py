3dc88f1eb956a4aee5451888e58e0acbaf3fdab1
Max Frei <max.frei@uni-due.de>
2021-02-24 09:48:12 +0100

    Improved and consolidated data preparation script for  SOPAT catalyst dataset.

---
+++ data_preparation/sopat_catalyst.py
@@ -0,0 +1,194 @@
+import random
+import shutil
+from os import PathLike
+from pathlib import Path
+from typing import Union
+
+import cv2
+import fire
+import numpy as np
+import pandas as pd
+from PIL import Image
+from tqdm import tqdm
+
+AnyPath = Union[str, bytes, PathLike]
+
+
+def split(data_root: AnyPath, validation_percentage: float, seed: int = 42):
+    """Split data into a training and a validation set.
+
+    :param data_root: Path, where images and mask folders are stored.
+    :param validation_percentage: Percentage of images to put into the validation set.
+    :param seed: random seed
+    :return:
+    """
+
+    if not validation_percentage <= 1:
+        raise ValueError("`validation_percentage` must be in the range [0, 1].")
+
+    random.seed(seed)
+    data_root = Path(data_root)
+
+    validation_folder_path = data_root / "validation"
+    validation_folder_path.mkdir(parents=True, exist_ok=True)
+
+    training_folder_path = data_root / "training"
+    training_folder_path.mkdir(parents=True, exist_ok=True)
+
+    image_paths = list(data_root.glob("image_*.png"))
+    num_images = len(image_paths)
+
+    num_images_validation = round(validation_percentage * num_images)
+    image_paths_validation = random.sample(image_paths, num_images_validation)
+
+    _move_files(data_root, image_paths_validation, validation_folder_path)
+
+    image_paths_training = data_root.glob("image_*.png")
+
+    _move_files(data_root, image_paths_training, training_folder_path)
+
+
+def _move_files(data_root, image_paths, subset_folder_path):
+    for image_path in image_paths:
+        image_id = image_path.stem[6:]
+        mask_paths = data_root.glob(f"**/mask_{image_id}*.*")
+
+        for mask_path in mask_paths:
+            class_name = mask_path.parent.name
+            new_mask_path = subset_folder_path / class_name / mask_path.name
+            new_mask_path.parent.mkdir(parents=True, exist_ok=True)
+
+            shutil.move(mask_path, new_mask_path)
+
+        shutil.move(image_path, subset_folder_path / image_path.name)
+
+
+def convert_imagej_csv_to_masks_batch(
+    data_root: AnyPath,
+    image_folder: AnyPath,
+    csv_file_glob: AnyPath,
+    output_root: AnyPath,
+    mask_folder_name: str,
+) -> None:
+    """Convert circular/elliptical ImageJ annotations to binary masks. Also converts and renames
+        associated input images to png.
+
+    :param data_root: Path, where ImageJ results csv-file and associated images are stored.
+    :param image_folder: Path of the folder where the input images are stored, relative to data_root
+    :param csv_file_glob: glob expression to identify ImageJ csv files, relative to the data_root.
+    :param output_root: Path of the folder, were resulting binary masks and images are stored,
+        relative to data_root.
+    :param mask_folder_name: name of the folder, where masks are stored. Will be created in
+        output_root.
+    """
+
+    data_root = Path(data_root)
+
+    for csv_file_path in tqdm(data_root.glob(csv_file_glob)):
+        convert_imagej_csv_to_masks(
+            data_root, image_folder, csv_file_path, output_root, mask_folder_name
+        )
+
+
+def convert_imagej_csv_to_masks(
+    data_root: AnyPath,
+    image_folder: AnyPath,
+    csv_path: AnyPath,
+    output_root: AnyPath,
+    mask_folder_name: str,
+) -> None:
+    """Convert circular/elliptical ImageJ annotations to binary masks. Also converts and renames
+        associated input images to png.
+
+    :param data_root: Path, where ImageJ results csv-file and associated images are stored.
+    :param image_folder: Path of the folder where the input images are stored, relative to data_root
+    :param csv_path: Path of the ImageJ results csv-file, relative to data_root.
+    :param output_root: Path, where the output mask folder and images reside.
+    :param mask_folder_name: name of the folder, where masks are stored. Will be created in
+        output_root.
+    """
+
+    data_root = Path(data_root)
+    output_root = Path(output_root)
+
+    mask_folder_path = output_root / mask_folder_name
+    mask_folder_path.mkdir(exist_ok=True, parents=True)
+
+    csv_data = pd.read_csv(csv_path)
+
+    image_file_names = csv_data["Label"].unique()
+
+    for image_file_name in image_file_names:
+        annotation_data = csv_data[csv_data["Label"] == image_file_name]
+
+        image_path = data_root / image_folder / image_file_name
+        image_name = Path(image_file_name).stem
+        image = Image.open(image_path)
+        image.save(output_root / f"image_{image_name}.png")
+
+        for annotation_index, annotation in annotation_data.iterrows():
+            mask = np.zeros_like(image)
+
+            center = (int(annotation["X"]), int(annotation["Y"]))
+            axes = (int(annotation["Major"] / 2), int(annotation["Minor"] / 2))
+            angle = annotation["Angle"] + 90
+
+            mask = cv2.ellipse(mask, center, axes, angle, 0, 360, color=1, thickness=-1)
+            mask = Image.fromarray(mask.astype(bool))
+            mask.save(mask_folder_path / f"mask_{image_name}_{annotation_index}.png")
+
+
+def convert_sopat_csv_to_masks(
+    data_root: AnyPath, image_folder: AnyPath, csv_file: AnyPath, output_folder: AnyPath
+) -> None:
+    """Convert circular/elliptical ImageJ annotations to binary masks. Also converts and renames
+        associated input images to png.
+
+    :param data_root: Path, where ImageJ results csv-file and associated images are stored.
+    :param image_folder: Path of the folder where the input images are stored, relative to data_root.
+    :param csv_file: Path of the ImageJ results csv-file, relative to data_root.
+    :param output_folder: Path of the folder, were resulting binary masks and images are stored,
+        relative to data_root.
+    """
+
+    data_root = Path(data_root)
+    output_path = data_root / output_folder
+    output_path.mkdir(exist_ok=True, parents=True)
+
+    csv_path = data_root / csv_file
+
+    with open(csv_path, "r") as file:
+        csv_data_raw = file.read()
+
+    csv_blocks = csv_data_raw.split("FILE: ")[1:]
+
+    for csv_block in csv_blocks:
+        lines = csv_block.split("\n")
+        lines = [line for line in lines if line != ""]
+
+        image_file_name = lines[0].split("\\")[-1]
+
+        # Extract relevant lines.
+        annotations = lines[2:-2]
+        annotations = [mask_parameters.split(";")[:-1] for mask_parameters in annotations]
+        annotations = pd.DataFrame(annotations, columns=["r", "x", "y"])
+        annotations = annotations.astype(float).astype(int)
+
+        image_path = data_root / image_folder / image_file_name
+        image_name = Path(image_file_name).stem
+        image = Image.open(image_path)
+
+        for annotation_index, annotation in annotations.iterrows():
+            mask = np.zeros_like(image)
+
+            center = (annotation["x"], annotation["y"])
+            axes = (annotation["r"], annotation["r"])
+            angle = 0
+
+            mask = cv2.ellipse(mask, center, axes, angle, 0, 360, color=1, thickness=-1)
+            mask = Image.fromarray(mask.astype(bool))
+            mask.save(output_path / f"mask_{image_name}_{annotation_index}.png")
+
+
+if __name__ == "__main__":
+    fire.Fire()
